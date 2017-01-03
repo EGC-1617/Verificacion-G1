b2a3094d9bc22fd36b0189d80865847e4c9d7d80
josruinav <jose.9.viso@gmail.com>
2017-01-03 14:25:55 +0100

    Optimización del metodo sqrt de la clase curve.java

--- src/main/java/main/java/Curve.java
+++ src/main/java/main/java/Curve.java
@@ -58,19 +58,6 @@ public abstract class Curve {
 	protected boolean checkPrime() {
 		return n.isProbablePrime(15);
 	}
-
-	protected static BigInteger sqrt(BigInteger n) {
-		BigInteger a = BigInteger.ONE;
-		BigInteger b = new BigInteger(n.shiftRight(5).add(new BigInteger("8")).toString());
-		while (b.compareTo(a) >= 0) {
-			BigInteger mid = new BigInteger(a.add(b).shiftRight(1).toString());
-			if (mid.multiply(mid).compareTo(n) > 0)
-				b = mid.subtract(BigInteger.ONE);
-			else
-				a = mid.add(BigInteger.ONE);
-		}
-		return a.subtract(BigInteger.ONE);
-	}
 	
 	
 	public BigInteger ordre(PointGMP p1) {
@@ -92,6 +79,19 @@ public abstract class Curve {
 		}
 
 		return m;
+	}
+	
+	protected static BigInteger sqrt(BigInteger n) {
+		BigInteger a = BigInteger.ONE;
+		BigInteger b = new BigInteger(n.shiftRight(5).add(new BigInteger("8")).toString());
+		while (b.compareTo(a) >= 0) {
+			BigInteger mid = new BigInteger(a.add(b).shiftRight(1).toString());
+			if (mid.multiply(mid).compareTo(n) > 0)
+				b = mid.subtract(BigInteger.ONE);
+			else
+				a = mid.add(BigInteger.ONE);
+		}
+		return a.subtract(BigInteger.ONE);
 	}
 
 	public List<BigInteger> decompositionPremier() {
