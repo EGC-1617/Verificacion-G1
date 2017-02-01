e0bdd767ad8d612c17ab5e34faafdb1b7d515d2e
Macarena Rufo <mrufpal@gmail.com>
2017-02-01 12:33:48 +0100

    Correccion metodos RSA

--- src/main/java/main/java/CryptoRSA.java
+++ src/main/java/main/java/CryptoRSA.java
@@ -1,94 +1,84 @@
 package main.java;
 
+import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
 
 import javax.crypto.Cipher;
 
 public class CryptoRSA {
 
-	
 	private static Cipher rsa;
-	
-	
+
 	/**
 	 * Dado un voto, generamos su clave pública y privada
 	 * 
-	 * @param vote : Voto sin encriptar
+	 * @param vote
+	 *            : Voto sin encriptar
 	 * @return Par de claves pública y privada
 	 * @throws NoSuchAlgorithmException
 	 */
-	public static KeyPair generateKeyPair(String vote) throws NoSuchAlgorithmException {
+	public static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
 		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
 		KeyPair keyPair = keyPairGenerator.generateKeyPair();
 
 		return keyPair;
 	}
-	
+
 
 	/**
-	 * Método que dado un voto y una clave pública, encripta usando RSA
-	 * 
-	 * @param vote : Voto sin encriptar
-	 * @param key : Clave publica
+	 *  Método que dado un voto y una clave pública, encripta usando RSA
+	 *  
+	 * @param textToEncypt
+	 *            : Voto sin encriptar
+	 * @param publicKey
+	 *            : Clave publica
 	 * @return Voto encriptado
 	 */
-	public static String encrypt (String vote, PublicKey key) {
-
-		String cipherVoteString = null;
-		byte[] cipherVote = null;
+	public static byte[] encrypt(byte[] textToEncypt, Key publicKey) {
+		byte[] cipherText = new byte[0];
 
 		try {
 			// Obtener el tipo de encriptacion que vamos a realizar
 			rsa = Cipher.getInstance("RSA");
 			
 			// Encripta
-			rsa.init(Cipher.ENCRYPT_MODE, key);
-			cipherVote = rsa.doFinal(vote.getBytes());
-
-			// Convertimos a String el voto encriptado
-			cipherVoteString = new String(cipherVote);
+			rsa.init(Cipher.ENCRYPT_MODE, publicKey);
+			cipherText = rsa.doFinal(textToEncypt);
 
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("encrypt exception: " + e.getMessage());
 		}
 
-		return cipherVoteString;
+		return cipherText;
 	}
-	
-	
+
 	/**
 	 * Método que dado un voto y una clave privada, desencripta usando RSA
 	 * 
-	 * @param cipherText : Voto encriptado
-	 * @param key : Clave privada
+	 * @param cipherText
+	 *            : Voto encriptado
+	 * @param privateKey
+	 *             : Clave privada
 	 * @return Voto desencriptado
 	 */
-	public static String decrypt (String cipherText, PrivateKey key) {
-
-		String vote = null;
-		byte[] dectyptedVote = null;
+	public static byte[] decrypt (byte[] cipherText, Key privateKey) {
+		byte[] text = new byte[0];
 
 		try {
 			// Obtener el tipo de encriptacion que vamos a realizar
 			rsa = Cipher.getInstance("RSA");
 			
 			// Desencripta
-			rsa.init(Cipher.DECRYPT_MODE, key);
-			dectyptedVote = rsa.doFinal(cipherText.getBytes());
-
-			// Convertimos a String el voto desencriptado
-			vote = new String(dectyptedVote);
+			rsa.init(Cipher.DECRYPT_MODE, privateKey);
+			text = rsa.doFinal(cipherText);
 
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("decrypt exception: " + e.getMessage());
 		}
 
-		return vote;
+		return text;
 	}
-
 
 }
